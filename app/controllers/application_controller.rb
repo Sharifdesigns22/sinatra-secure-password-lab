7cf20846b2789c99a9062baf63131243ed7b9575
Sharif Tarver <Shariftarver@gmail.com>
2021-03-25 01:59:41 -0700

    commit tree 5dff528ab54c78ae00bbf068cebb2eee050253c5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,14 @@
 require "./config/environment"
 require "./app/models/user"
 class ApplicationController < Sinatra::Base
-
   configure do
     set :views, "app/views"
     enable :sessions
     set :session_secret, "password_security"
   end
-
   get "/" do
     erb :index
   end
-
   get "/signup" do
     erb :signup
   end
@@ -19,39 +16,45 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     #your code here
 
+    if params[:username]=="" || params[:password]==""
+      redirect '/failure'
+    else
+      redirect "/login"
+    end
   end
 
   get '/account' do
     @user = User.find(session[:user_id])
     erb :account
   end
-
-
   get "/login" do
     erb :login
   end
 
   post "/login" do
     ##your code here
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password]) #if we found user by username and user's pw matches up with value in password_digest
+    session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
   end
 
   get "/failure" do
     erb :failure
   end
-
   get "/logout" do
     session.clear
     redirect "/"
   end
-
   helpers do
     def logged_in?
       !!session[:user_id]
     end
-
     def current_user
       User.find(session[:user_id])
     end
   end
-
 end
